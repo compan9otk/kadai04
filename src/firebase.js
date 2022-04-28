431a700af152ac40ae3f60d252c9fc9abad0e126
compan9otk <6ri6rikatatu6ri@gmail.com>
2022-04-29 08:28:19 +0900

    firebaseの情報を削除

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDm7uAiAzMBE5thEh0FxMHbLqYe42_74AA",
-    authDomain: "twitter-clone-713fe.firebaseapp.com",
-    projectId: "twitter-clone-713fe",
-    storageBucket: "twitter-clone-713fe.appspot.com",
-    messagingSenderId: "772032420562",
-    appId: "1:772032420562:web:a7bd1af5d540a851be6b0b"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const app = initializeApp(firebaseConfig);
